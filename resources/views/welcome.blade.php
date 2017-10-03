b56b126d3a7bee2bc66240c72800e6f9998dcfcc
Tim Thomas <thomases@comcast.net>
2017-10-03 18:49:31 -0400

    imstarboard (coolscience) incremental update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>Starboard Technology Partners</title>
+    <title>Starboard Technology Partner</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +19,7 @@
   <body>
 
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#">Starboard Technology Partners</a>
+      <a class="navbar-brand" href="#">Starboard Technology Partner</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -50,14 +50,14 @@
       </ol-->
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="first-slide d-none d-md-inline-block" src="images/home/Happy-Small-Business-Owner.jpg" alt="First slide">
-          <img class="first-slide d-md-none d-inline-block" src="images/home/Happy-Small-Business-Owner-small.jpg" alt="First slide">
+          <img class="first-slide d-none d-md-inline-block" src="images/home/Happy-Small-Business-Owner.jpg" alt="Small Business Owner">
+          <img class="first-slide d-md-none d-inline-block" src="images/home/Happy-Small-Business-Owner-small.jpg" alt="Small Business Owner mobile">
           <div class="container">
             <div class="carousel-caption d-block text-left">
               <div style="height: 50px;" class="d-none d-md-inline-block"><h1 style="color: black;">Finding great Technical help</h1></div>
-              <p>Does it feel like reasonable, quality professional technical<br>help is always out of reach?</p>
+              <p>Why does it always feel like quality, professional technical<br>help is just out of reach?</p>
               <br>
-              <p><a style="margin-top: 10px;" id="btn-1" class="btn btn-lg btn-primary d-none d-md-inline-block" data-slide-to="1" data-target="#myCarousel" href="#" role="button">...</a></p>
+              <p><a style="margin-top: 10px;" id="btn-1" class="btn btn-lg btn-primary d-none d-md-inline-block" data-slide-to="1" data-target="#myCarousel" href="#" role="button">Why?</a></p>
             </div>
           </div>
         </div>
@@ -72,12 +72,16 @@
           </div>
         </div-->
         <div class="carousel-item">
-          <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+          <img class="third-slide d-none d-md-inline-block" src="images/home/business-man.jpg" alt="Small Business Owner">
+          <img class="third-slide d-md-none d-inline-block" src="images/home/business-man-small.jpg" alt="Small Business Owner mobile">
           <div class="container">
-            <div class="carousel-caption d-block text-center">
-              <h1>TRUST</h1>
-              <p>Are you BEHIND on the technical projects that will differentiate your company <br>because you don't have CONFIDENCE in the technical people you are using?</p>
-              <p><a id="btn-3" class="btn btn-lg btn-primary d-none d-md-inline-block" href="#" role="button">Browse gallery</a></p>
+            <div class="carousel-caption d-block text-left">
+              <div style="height: 50px;" class="d-none d-md-inline-block"><h1 style="color: black;">TRUST</h1></div>
+              <p class="d-none d-md-inline-block">I need to know that my technical needs will be<br>met on time and on budget.</p>
+              <div class="d-md-none d-inline-block bg-white">
+                <p class="text-black">I need to know that my<br>technical needs will be met<br>on time and on budget.</p>
+              </div>
+              <p><a id="btn-3" class="btn btn-lg btn-primary d-none d-md-inline-block" href="#" role="button">Better way</a></p>
             </div>
           </div>
         </div>
@@ -99,24 +103,35 @@
 
     <div class="container marketing">
 
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">As your partner, our goal is for YOU <span class="text-muted">to succeed.</span></h2>
+          <p class="lead">We think of ourselves as partners, because when you take us on, we take your goals, timelines and your budget seriously.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
       <!-- Three columns of text below the carousel -->
+      <div class="row">&nbsp;</div>
       <div class="row">
         <div class="col-lg-4">
           <img class="rounded-circle" src="images/home/businesswoman-140x140.jpg" alt="Generic placeholder image" width="140" height="140">
           <h2><img class="logo30h" src="images/home/salesforce-logo-30x42.png" alt="Salesforce" />Salesforce</h2>
-          <p><span class="quotes">&ldquo;</span>Tim implemented our new Salesforce Lightning sales app on time and on budget.<span class="quotes">&rdquo;</span></p>
+          <p class="text-justify fixh"><span class="quotes">&ldquo;</span>They implemented our new Salesforce Lightning sales app on time and on budget. It's such a relief to have that feeling that they are in this with us.<span class="quotes">&rdquo;</span></p>
           <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="rounded-circle" src="images/home/frank-140x140.jpg" alt="Generic placeholder image" width="140" height="140">
           <h2><img class="logo30h" src="images/home/Shopify-Logo-25x30.png" alt="Shopify" />Shopify</h2>
-          <p><span class="quotes">&ldquo;</span>A colleague I trust recommended Tim to me as honest, professional and reasonable, and that's exactly what I got.<span class="quotes">&ldquo;</span></p>
+          <p class="text-justify fixh"><span class="quotes">&ldquo;</span>A colleague I trust recommended Starboard to me as honest, professional and reasonable, and that's exactly what I got. I can relax knowing that I can trust them as a business partner.  It frees up time to focus on other parts of my business, among other things.<span class="quotes">&rdquo;</span></p>
           <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="rounded-circle" src="images/home/businessman-140x140.jpeg" alt="Generic placeholder image" width="140" height="140">
           <h2><img class="logo30h" src="images/home/agile-scrum-48x30.png" alt="Agile Scrum" />Agile</h2>
-          <p><span class="quotes">&ldquo;</span>Tim optimized our 3 virtual development teams, mentoring and optimizing agile and continuous delivery. We say drammatic improvements in overall efficiency and quality.<span class="quotes">&ldquo;</span></p>
+          <p class="text-justify fixh"><span class="quotes">&ldquo;</span>They optimized our 3 virtual development teams, mentoring and optimizing agile and continuous delivery. We say drammatic improvements in overall efficiency and quality.<span class="quotes">&rdquo;</span></p>
           <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
@@ -127,20 +142,8 @@
       <hr class="featurette-divider">
 
       <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
         <div class="col-md-7 order-md-2">
-          <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+          <h2 class="featurette-heading">As a trusted partner, you can trust us to worry about the tech <span class="text-muted">You can focus on the rest of your business.</span></h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
         <div class="col-md-5 order-md-1">
@@ -152,7 +155,7 @@
 
       <div class="row featurette">
         <div class="col-md-7">
-          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+          <h2 class="featurette-heading">Give us a <span class="text-muted">test-drive.</span></h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
         <div class="col-md-5">
@@ -168,7 +171,7 @@
       <!-- FOOTER -->
       <footer>
         <p class="float-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2017 Starboard Technology &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p>&copy; 2017 Starboard Technology &middot; <a href="#">Contact</a> <!--&middot; <a href="#">Terms</a></p-->
       </footer>
 
     </div><!-- /.container -->
@@ -184,8 +187,8 @@
         crossorigin="anonymous">
     </script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/vendor/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/vendor/popper.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
     <script src="js/vendor/holder.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
@@ -195,8 +198,25 @@
         #btn-1{ position:absolute; top:125px; }​
         .ctr{text-align: center;}
         .carousel img { height: auto; }
-        .quotes{font-family: ariel; font-size: 20px;}
+        .quotes{
+            font-family: ariel;
+            font-size: 35px;
+            position: relative;
+            top: 7px;
+            padding: 1px;
+        }
+        .fixh {line-height: 25px;}
         img.logo30h{position: relative; top: -4px; left: -2px;}
+        p.text-black{color: #000000;}
+        div.bg-white {
+            opacity: 0.75; 
+            filter: alpha(opacity=75); /* For IE8 and earlier */
+            margin: 2px;
+            text-align: center;
+            border-radius: 5px;
+            -moz-border-radius: 5px;
+            -webkit-border-radius: 5px;
+        }
     </style>
 
     <script src="js/detectmobilebrowser.js"></script>
@@ -213,21 +233,17 @@
                 var counter = 1;
                 $('#btn-1').mouseover(function() {
                     switch(counter) {
-                        // case 0:
-                        //     var newT = 450;
-                        //     var newL = 645;
-                        //     $('#btn-1').css('background-color', '#17028e');
-                        //     $('#btn-1').css('border', '1px solid #17028e');
-                        //     break;
                         case 1:
                             var newT = 510;
                             var newL = 365;
+                            $('#btn-1').text('Almost had it...');
                             $('#btn-1').css('background-color', '#ff8800');
                             $('#btn-1').css('border', '1px solid #ff8800');
                             break;
                         case 2:
                             var newT = 430;
                             var newL = 215;
+                            $('#btn-1').text('Ugh!');
                             $('#btn-1').css('background-color', 'red');
                             $('#btn-1').css('border', '1px solid red');
                             break;
@@ -249,7 +265,7 @@
 
                 $('#btn-3').click(function() {
                     $('.marketing').show();
-                    $('html, body').animate({scrollTop: '+=500px'}, 800);
+                    $('html, body').animate({scrollTop: '+=505px'}, 800);
                     $('body').css('background-color', '#ffffff');
                 });
             };
